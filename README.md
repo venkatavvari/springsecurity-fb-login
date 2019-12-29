19ebe2eb6a9246b05deb71bae2beefe659ecae0d
venkat_avvari <venkat.avvari1@gmail.com>
2019-12-30 00:22:56 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# springsecurity-fb-login
---
+++ src/main/java/io/javabrains/springsecurityfblogin/SpringsecurityFbLoginApplication.java
@@ -0,0 +1,13 @@
+package io.javabrains.springsecurityfblogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityFbLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecurityFbLoginApplication.class, args);
+    }
+
+}
